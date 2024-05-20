b6977358886d42e8ee8b08dfdb77915275497e1c
Tri Tong <tmtong@usc.edu>
2024-05-20 12:47:05 -0700

    Configured api folder and deployed addMessage function to cloud

---
+++ functions/api/addMessage.js
@@ -0,0 +1,49 @@
+// Writtem in CJS
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const { logger } = functions;
+
+exports.addMessage = functions.https.onCall(async (data, context) => {
+  try {
+    logger.log("Received message request data:", data);
+
+    // Validate required fields
+    if (!data.text || !data.userId) {
+      logger.log("Required fields (text or userId) are missing");
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "Required fields (text or userId) are missing"
+      );
+    }
+
+    const { text, userId } = data;
+
+    // Construct message data
+    const messageData = {
+      text,
+      userId,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    // Add message to user's message subcollection in Firestore
+    const messageRef = await admin
+      .firestore()
+      .collection("chats")
+      .doc(userId)
+      .collection("messages")
+      .add(messageData);
+
+    logger.log("Message successfully added, message ID:", messageRef.id);
+
+    // return success and message ID
+    return { status: "success", messageId: messageRef.id };
+  } catch (error) {
+    logger.error("Error adding message:", error);
+    // Throw a structured error for client
+    throw new functions.https.HttpsError(
+      "unknown",
+      "An error occured while adding the message",
+      error.message
+    );
+  }
+});
